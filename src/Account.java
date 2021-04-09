afba9fa7e93ca892610197fe0f291ef143a4c23a
isa duran <82124466+isaduran34@users.noreply.github.com>
2021-04-09 10:50:43 -0400

    Update Account.java

--- src/Account.java
+++ src/Account.java
@@ -6,10 +6,10 @@
  */
 
 public abstract class Account implements BaseInterestRate{
-	String name;
-	String ssn;
-	double balance;
-	double rate;
+	private String name;
+	private String ssn;
+	private double balance;
+	private double rate;
 	
 	
 	/*
@@ -41,7 +41,7 @@ public abstract class Account implements BaseInterestRate{
 		this.name = name;
 		this.ssn = ssn;
 		this.balance = balance;
-		rate = 0.5;
+		
 	}
 	
 	
